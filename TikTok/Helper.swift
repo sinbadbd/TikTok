c2f54234bb532f46d11968c29f33ffd1f9795a66
imran <imran.mia@portonics.com>
2021-03-20 18:11:48 +0600

    preview camera

---
+++ TikTok/Helper.swift
@@ -0,0 +1,36 @@
+//
+//  Helper.swift
+//  TikTok
+//
+//  Created by Imran on 20/3/21.
+//
+
+import UIKit
+
+
+extension UIColor {
+    var imageRepresentation : UIImage {
+      let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+      UIGraphicsBeginImageContext(rect.size)
+      let context = UIGraphicsGetCurrentContext()
+
+      context?.setFillColor(self.cgColor)
+      context?.fill(rect)
+
+      let image = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+
+    return image!
+  }
+}
+
+
+extension UIButton{
+
+    func setImageTintColor(_ color: UIColor) {
+        let tintedImage = self.imageView?.image?.withRenderingMode(.alwaysTemplate)
+        self.setImage(tintedImage, for: .normal)
+        self.tintColor = color
+    }
+
+}
